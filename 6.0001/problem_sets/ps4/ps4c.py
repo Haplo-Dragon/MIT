69e200fd6e8c20ed0b9c9619d401ce536b84dfe7
Ethan Fulbright <haplodragon@gmail.com>
2018-10-14 00:23:20 -0500

    Solve Part C - Part 3 - Testing

--- 6.0001/problem_sets/ps4/ps4c.py
+++ 6.0001/problem_sets/ps4/ps4c.py
@@ -217,4 +217,20 @@ if __name__ == "__main__":
     enc_message = EncryptedSubMessage(message.apply_transpose(enc_dict))
     print("Decrypted message:", enc_message.decrypt_message())
 
-    # TODO: WRITE YOUR TEST CASES HERE
+    message = SubMessage("Hello, fat cat!")
+    permutation = "iuoea"
+    enc_dict = message.build_transpose_dict(permutation)
+    print("Original message:", message.get_message_text(), "Permutation:", permutation)
+    print("Expected encryption:", "Hulle, fit cit!")
+    print("Actual encryption:", message.apply_transpose(enc_dict))
+    enc_message = EncryptedSubMessage(message.apply_transpose(enc_dict))
+    print("Decrypted message:", enc_message.decrypt_message())
+
+    message = SubMessage("Hi, muscles!")
+    permutation = "uoiae"
+    enc_dict = message.build_transpose_dict(permutation)
+    print("Original message:", message.get_message_text(), "Permutation:", permutation)
+    print("Expected encryption:", "Hi, mesclos!")
+    print("Actual encryption:", message.apply_transpose(enc_dict))
+    enc_message = EncryptedSubMessage(message.apply_transpose(enc_dict))
+    print("Decrypted message:", enc_message.decrypt_message())
